bdcd2e1bc24633d03397286d32227981bb50d3ea
Joseph Lee <joseph.lee22590@gmail.com>
2014-09-04 13:18:34 -0700

    Version 1.1 is ready.

--- buildVars.py
+++ buildVars.py
@@ -17,7 +17,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on installation and add-on information
 	"addon-description" : _("""Add-on for using NVDA as a feed reader."""),
 	# version
-	"addon-version" : "1.1-dev",
+	"addon-version" : "1.1",
 	# Author(s)
 	"addon-author" : u"Noelia Ruiz Martínez <nrm1977@gmail.com>, Mesar Hameed <mhameed@src.gnome.org>",
 	# URL for the add-on documentation support
