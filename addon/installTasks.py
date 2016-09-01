eb1e60a10002d5f55a82eb6eef0139a29560051a
Noelia Ruiz Martínez <nrm1977@gmail.com>
2016-09-01 22:02:40 +0200

    Added header on installTasks

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -1,4 +1,7 @@
-import addonHandler
+# -*- coding: UTF-8 -*-
+#Copyright (C) 2013-2016 Noelia Ruiz Martínez, other contributors
+# Released under GPL2import addonHandler
+
 import globalVars
 import os
 import shutil
