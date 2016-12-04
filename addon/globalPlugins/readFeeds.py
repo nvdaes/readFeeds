a3686d09be2cb04867ea965f74a760ba14e5411c
Noelia Ruiz Martínez <nrm1977@gmail.com>
2016-12-04 20:06:36 +0100

    Added the option to set default addressfiles in configuration profiles, to use with plugin scripts.

--- addon/globalPlugins/readFeeds.py
+++ addon/globalPlugins/readFeeds.py
@@ -18,6 +18,7 @@ import os
 import sys
 import shutil
 import globalVars
+import config
 import urllib
 import scriptHandler
 import api
@@ -34,14 +35,21 @@ del sys.path[-1]
 addonHandler.initTranslation()
 
 ### Constants
+
 ADDON_DIR = os.path.join(os.path.dirname(__file__), "..")  # The root of the addon folder
 ADDON_INSTANCE = addonHandler.Addon(ADDON_DIR)
 ADDON_SUMMARY = ADDON_INSTANCE.manifest['summary']
 FEEDS_PATH = os.path.join(ADDON_DIR, "globalPlugins", "personalFeeds")
 CONFIG_PATH = globalVars.appArgs.configPath
-
 # Translators: message presented when feeds cannot be reported.
 CAN_NOT_REPORT = _("Unable to refresh feed. Check your Internet conectivity or that the specified feed address is correct.")
+
+### Configuration
+
+confspec = {
+	"addressFile": "string(default="")",
+}
+config.conf.spec["readFeeds"] = confspec
 
 ### Dialogs
 
@@ -75,7 +83,7 @@ class FeedsDialog(wx.Dialog):
 		changeFeedsSizer.AddSpacer(guiHelper.SPACE_BETWEEN_BUTTONS_VERTICAL)
 
 		# Translators: The label of a button to open the list of articles of a feed.
-		self.articlesButton = wx.Button(self, label=_("List of articles..."))
+		self.articlesButton = wx.Button(self, label=_("List of &articles..."))
 		self.articlesButton.Bind(wx.EVT_BUTTON, self.onArticles)
 		self.AffirmativeId = self.articlesButton.Id
 		self.articlesButton.SetDefault()
@@ -138,7 +146,8 @@ class FeedsDialog(wx.Dialog):
 		self.articlesButton.Enabled = self.sel>= 0
 		self.deleteButton.Enabled = self.sel >= 0
 		self.renameButton.Enabled = self.sel >= 0
-		self.defaultButton.Enabled = self.sel >= 0
+		self.defaultButton.Enabled = (self.sel >= 0 and
+			self.stringSel != config.conf["readFeeds"]["addressFile"])
 
 	def onArticles(self, evt):
 		with open(os.path.join(FEEDS_PATH, "%s.txt" % self.stringSel), "r") as f:
@@ -146,17 +155,17 @@ class FeedsDialog(wx.Dialog):
 			f.close()
 		feed = Feed(address)
 		with wx.SingleChoiceDialog(self,
-			# Translators: Caption of a dialog.
-			_("Open an article"),
+			# Translators: the label of a single choice dialog.
+			_("Open web page of selected article."),
 			# Translators: Title of a dialog.
-			_("List of feeds (%d)" % feed.getNumberOfArticles()),
+			u"{feedTitle} ({feedNumber})".format(feedTitle=self.stringSel, feedNumber=feed.getNumberOfArticles()),
 			[feed.getArticleTitle(index) for index in xrange(feed.getNumberOfArticles())]) as d:
 			if d.ShowModal() == wx.ID_CANCEL:
 				return
 			os.startfile(feed.getArticleLink(d.Selection))
 
 	def onNew(self, evt):
-		# Translators: The label of a field to enter an adress for a new feed.
+		# Translators: The label of a field to enter an address for a new feed.
 		with wx.TextEntryDialog(self, _("Address of a new feed:"),
 			# Translators: The title of a dialog to create a new feed.
 				_("New feed")) as d:
@@ -180,7 +189,9 @@ class FeedsDialog(wx.Dialog):
 		self.onFeedsListChoice(None)
 
 	def onDefault(self, evt):
-		pass
+		config.conf["readFeeds"]["addressFile"] = self.stringSel
+		self.onFeedsListChoice(None)
+		self.feedsList.SetFocus()
 
 	def onRename(self, evt):
 		# Translators: The label of a field to enter a new name for a feed.
@@ -290,7 +301,8 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 		_("&Feeds..."),
 		# Translators: the tooltip for a menu item.
 		_("View and manage feeds"))
-		gui.mainFrame.sysTrayIcon.Bind(wx.EVT_MENU, self.onList, self.feedsListItem)
+		gui.mainFrame.sysTrayIcon.Bind(wx.EVT_MENU, self.onFeeds, self.feedsListItem)
+		self.feed = None
 
 	def terminate(self):
 		try:
@@ -298,33 +310,33 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 		except wx.PyDeadObjectError:
 			pass
 
-	def onList(self, evt):
+	def onFeeds(self, evt):
 		gui.mainFrame.prePopup()
 		d = FeedsDialog(gui.mainFrame)
 		d.Show()
 		gui.mainFrame.postPopup()
 
-	def onReadFirstFeed(self, evt):
-		try:
-			self._feed = Feed(address)
-		except Exception as e:
-			ui.message(cannotReport)
-			return
-		ui.message(self._feed.getArticleTitle())
-
-	def onReadFeedsFileManager(self, evt):
-		gui.mainFrame._popupSettingsDialog(ReadFeedsManagerDialog)
+	def getFirstFeed(self):
+		if not self.feed:
+			try:
+				addressFile = "%s.txt" % config.conf["readFeeds"]["addressFile"]
+				with open(os.path.join(FEEDS_PATH, addressFile), "r") as f:
+					address = f.read()
+					f.close()
+				self.feed = Feed(address)
+			except Exception as e:
+				ui.message(CAN_NOT_REPORT)
 
 	def script_readFirstFeed(self, gesture):
-		self.onReadFirstFeed(None)
+		self.getFirstFeed()
+		if self.feed:
+			ui.message(self.feed.getArticleTitle())
 	# Translators: message presented in input mode.
 	script_readFirstFeed.__doc__ = _("Refreshes the current feed and announces the most recent article title.")
 
 	def script_readCurrentFeed(self, gesture):
-		if not self._feed:
-			ui.message(_("Refresh your selected feed to read the current article."))
-			return
-		feedInfo = u"{title}\r\n\r\n{address}".format(title=self._feed.getArticleTitle(), address=self._feed.getArticleLink())
+		self.getFirstFeed()
+		feedInfo = u"{title}\r\n\r\n{address}".format(title=self.feed.getArticleTitle(), address=self.feed.getArticleLink())
 		if scriptHandler.getLastScriptRepeatCount()==1 and api.copyToClip(feedInfo):
 			# Translators: message presented when the information about an article of a feed is copied to the clipboard.
 			ui.message(_("Copied to clipboard %s") % feedInfo)
@@ -334,28 +346,22 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 	script_readCurrentFeed.__doc__ = _("Announces the title of the current article. Pressed two times, copies title and related link to the clipboard.")
 
 	def script_readNextFeed(self, gesture):
-		if not self._feed:
-			ui.message(cannotReport)
-			return
-		self._feed.next()
-		ui.message(self._feed.getArticleTitle())
+		self.getFirstFeed()
+		self.feed.next()
+		ui.message(self.feed.getArticleTitle())
 	# Translators: message presented in input mode.
 	script_readNextFeed.__doc__ = _("Announces the title of the next article.")
 
 	def script_readPriorFeed(self, gesture):
-		if not self._feed:
-			ui.message(cannotReport)
-			return
-		self._feed.previous()
-		ui.message(self._feed.getArticleTitle())
+		self.getFirstFeed()
+		self.feed.previous()
+		ui.message(self.feed.getArticleTitle())
 	# Translators: message presented in input mode.
 	script_readPriorFeed.__doc__ = _("Announces the title of the previous article.")
 
 	def script_reportLink(self, gesture):
-		if not self._feed:
-			ui.message(cannotReport)
-			return
-		feedLink = self._feed.getArticleLink()
+		self.getFirstFeed()
+		feedLink = self.feed.getArticleLink()
 		if scriptHandler.getLastScriptRepeatCount()==1:
 			os.startfile(feedLink)
 		else:
@@ -363,58 +369,12 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 	# Translators: message presented in input mode.
 	script_reportLink.__doc__ = _("Announces article link, when pressed two times, opens related web page.")
 
-	def setAddressDialog(self):
-		d = wx.TextEntryDialog(gui.mainFrame,
-		# Translators: the label of a text entry dialog.
-		_("Feed address to follow"),
-		# Translators: the title of a text entry dialog.
-		_("Feed address"),
-		defaultValue=address)
-		def callback(result):
-			if result == wx.ID_OK:
-				# Make sure this happens after focus returns to the document.
-				wx.CallLater(100, self.doSetAddress, d.GetValue())
-		gui.runScriptModalDialog(d, callback)
-
-	def doSetAddress(self, text):
-		if not text:
-			return
-		global address
-		address=text
-		self._feed = Feed(address)
-		# Translators: message presented when the address of a feed has been selected.
-		ui.message(_("Selected %s") % address)
-
-	def script_setAddress(self, gesture):
-		self.onSetAddress(None)
-	# Translators: message presented in input mode.
-	script_setAddress.__doc__ = _("Opens a dialog for entering a feed address.")
-
-	def doSetAddressFile(self, file):
-		if not file:
-			return
-		global address
-		try:
-			f = open (file, "r")
-			address = f.read()
-			f.close()
-			# Translators: message presented when the address of a feed has been selected.
-			ui.message(_("Selected %s") % address)
-		except IOError:
-			# Translators: message presented when unable to read feed address.
-			ui.message(_("Unable to read feed address, feed can not be selected"))
-			return
-		self._Feed = Feed(address)
-
-	def script_setAddressFile(self, gesture):
-		self.onSetAddressFile(None)
-	# Translators: message presented in input mode.
-	script_setAddressFile.__doc__ = _("Opens a dialog for selecting a file containing a feed address.")
-
-	def script_saveAddress(self, gesture):
-		self.onSaveAddress(None)
-	# Translators: message presented in input mode.
-	script_saveAddress.__doc__ = _("Opens a dialog where you can enter a filename to which the current feed address will be saved.")
+	def script_copyFeedInfo(self, gesture):
+		self.getFirstFeed()
+		feedInfo = u"{title}\r\n\r\n{address}".format(title=self.feed.getArticleTitle(), address=self.feed.getArticleLink())
+		if api.copyToClip(feedInfo):
+			# Translators: message presented when the information about an article of a feed is copied to the clipboard.
+			ui.message(_("Copied to clipboard %s") % feedInfo)
 
 	__gestures = {
 		"kb:control+shift+NVDA+8": "readFirstFeed",
@@ -423,65 +383,6 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 		"kb:control+shift+NVDA+u": "readPriorFeed",
 		"kb:control+shift+NVDA+space": "reportLink",
 		"kb:control+shift+NVDA+enter": "setAddress",
-		"kb:control+NVDA+enter": "setAddressFile",
-		"kb:shift+NVDA+enter": "saveAddress",
+		"kb:shift+NVDA+enter": "copyFeedInfo",
 	}
 
-class ReadFeedsManagerDialog(gui.SettingsDialog):
-
-	title = _("Feeds manager")
-
-	def makeSettings(self, settingsSizer):
-		foldersSizer = wx.BoxSizer(wx.VERTICAL)
-		# Translators: the name of a dialog button.
-		self.focusedCtrl = wx.Button(self, label = _("&Backup personal feeds folder..."))
-		self.focusedCtrl.Bind(wx.EVT_BUTTON, self.onBrowseForBackupDirectory)
-		foldersSizer.Add(self.focusedCtrl)
-		# Translators: the name of a dialog button.
-		ctrl = wx.Button(self, label=_("R&estore personal feeds..."))
-		ctrl.Bind(wx.EVT_BUTTON, self.onBrowseForRestoreDirectory)
-		foldersSizer.Add(ctrl)
-		settingsSizer.Add(foldersSizer)
-
-	def postInit(self):
-		self.focusedCtrl.SetFocus()
-	def onBrowseForBackupDirectory(self, evt):
-		with wx.DirDialog(self,
-		# Translators: the label of a dialog to select a folder.
-		_("Select the folder where your personal feeds will be backed up."),
-		configPath) as dlg:
-			gui.mainFrame.prePopup()
-			if dlg.ShowModal() == wx.ID_OK:
-				gui.mainFrame.postPopup()
-				copyPath = os.path.join(dlg.GetPath(), "personalFeeds")
-				try:
-					shutil.rmtree(copyPath, ignore_errors=True)
-					shutil.copytree(_savePath, copyPath)
-				except WindowsError:
-					wx.CallAfter(gui.messageBox,
-					## Translators: the label of an error dialog.
-					_("Unable to backup folder"),
-					# Translators: the title of an error dialog.
-					_("Backup Error"),
-					wx.OK|wx.ICON_ERROR)
-
-	def onBrowseForRestoreDirectory(self, evt):
-		feedsPath = os.path.join(configPath, "personalFeeds")
-		with wx.DirDialog(self,
-		# Translators: the label of a dialog to select a folder.
-		_("Restore personal feeds from backup folder"),
-		feedsPath, wx.DD_DIR_MUST_EXIST  | wx.DD_DEFAULT_STYLE) as dlg:
-			gui.mainFrame.prePopup()
-			if dlg.ShowModal() == wx.ID_OK:
-				gui.mainFrame.postPopup()
-				feedsPath = dlg.GetPath()
-				try:
-					shutil.rmtree(_savePath, ignore_errors=True)
-					shutil.copytree(feedsPath, _savePath)
-				except WindowsError:
-					wx.CallAfter(gui.messageBox,
-					# Translators: the label of an error dialog.
-					_("Folder not restored"),
-					# Translators: the title of an error dialog.
-					_("Restore Error"),
-					wx.OK|wx.ICON_ERROR)
