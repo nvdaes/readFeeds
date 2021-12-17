9051ae7ebf90e2ec65a18b6f973c2446749eaf1a
Noelia Ruiz Martínez <nrm1977@gmail.com>
2021-12-17 08:22:38 +0100

    Fix opml path at installation

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -13,8 +13,6 @@ import glob
 ADDON_DIR = os.path.abspath(os.path.dirname(__file__))
 FEEDS_PATH = os.path.join(ADDON_DIR, "globalPlugins", "readFeeds", "personalFeeds")
 CONFIG_PATH = globalVars.appArgs.configPath
-OPML_PATH = os.path.join(FEEDS_PATH, "readFeeds.opml")
-
 addonHandler.initTranslation()
 
 
@@ -25,6 +23,7 @@ def onInstall():
 	)
 	if os.path.isdir(previousFeedsPath):
 		validFiles = glob.glob(previousFeedsPath + "\\*.txt")
+		validFiles.append(os.path.join(previousFeedsPath, "readFeeds.opml"))
 		validFiles.append(OPML_PATH)
 		for file in validFiles:
 			try:
