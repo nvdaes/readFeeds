74fa69fdf8a343b81c8320bb978088eb0f08038d
Noelia Ruiz Martínez <nrm1977@gmail.com>
2026-04-24 08:20:26 +0200

    Try to debug

--- .github/scripts/getAddonInfo.py
+++ .github/scripts/getAddonInfo.py
@@ -10,6 +10,7 @@ import buildVars
 
 
 def main():
+	print(os.getcwd)
 	addonId = buildVars.addon_info["addon_name"]
 	# Print only the addonId so it can be captured by PowerShell
 	print(addonId, end="", flush=True)
